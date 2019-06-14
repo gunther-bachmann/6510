4ed18da15c40f7de841af8e74ab2ecc862883252
Gunther Bachmann <gunther.bachmann@itemis.de>
2019-06-14 18:02:33 +0200

    add pure racket example

---
+++ 6510-example-rs.rkt
@@ -0,0 +1,44 @@
+#lang racket
+
+(require "6510.rkt")
+(require "6510-interpreter.rkt")
+
+(define org #xc000)
+
+(define program
+  (list
+   (LDA "#$41" )
+   (JSR ":COUT")
+   (ADC "#$01")
+   (JSR ":COUT")
+   (LDA "#%00001010") ; line feed
+   (JSR ":COUT")
+   (RTS)
+   (LABEL ":COUT")
+   (JSR "$FFD2")
+   (RTS)))
+
+(define addressing-program
+  (list
+   (LDA "#$40")           ;; immediate
+   (LDA "$40")            ;; zero-page
+   (LDA "$40",x)          ;; zero-page-x (add reg x to $40, read memory from there)
+   (LDA "$4000")          ;; absolute
+   (LDA "$4000",x)        ;; absolute x
+   (LDA "$4000",y)        ;; absolute y
+   (LDA < "$4000",x >)    ;; indirect x (add x to $4000, read pointer (low+high) from memory, read pointed to byte)
+   (LDA < "$4000" >,y)    ;; indirect y (read pointer from $4000, add y to pointer, read pointed to byte)
+   (LABEL ":NEXT")
+   (ASL A)                ;; work on accumulator
+   (BEQ "$10")            ;; relative to current program counter
+   (BEQ ":NEXT")))
+
+
+(define resolved-program (replace-labels program org))
+(define raw-bytes (commands->bytes org program))
+(define data (6510-load (initialize-cpu) org raw-bytes))
+(define executable-program (set-pc-in-state data org))
+
+;; (displayln "program execution:")
+;; (let ([_ (run executable-program))])
+;;   (void))
