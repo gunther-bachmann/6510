c695a2767fd984955798b464d9a453b92d3c7ea4
Gunther Bachmann <gunther.bachmann@itemis.de>
2022-08-16 05:38:58 +0200

    plan for next language features (ex/import, constants, asc)

--- 6510-example-rs.rkt
+++ 6510-example-rs.rkt
@@ -21,6 +21,13 @@
    (BNE ":NEXT")
    (BRK)
 
+   ;; (IMPORT-BYTE ":IMPORTEDBVAL")
+   ;; (IMPORT-WORD ":IMPORTEDWORD")
+   ;; (EXPORT-WORD ":EXPORTED" ":COUT"
+   ;; (CONSTANT-BYTE  ":MYVAL" "$42") ;; define label with byte constant: :myval = $42
+   ;; (CONSTANT-WORD  ":M16BVAL" "$C842") ;; define label with word constant: :m16bval = $c842
+   ;; (ASC "Hello World") ;; define bytes by ascii encoding: .asc "Hello World"
+
    (LABEL ("<label>") ":COUT")
    (JSR "$FFD2")
    (RTS)
