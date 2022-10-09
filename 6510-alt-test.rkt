20a33a3394bd8d983a4f390bad92eb95d9267eac
Gunther Bachmann <gunther.bachmann@itemis.de>
2022-10-09 22:18:24 +0200

    add smoketest for label use

--- 6510-alt-test.rkt
+++ 6510-alt-test.rkt
@@ -5,7 +5,10 @@
 (module+ test
   (require rackunit))
 
-(module+ test
+(module+ test #| smoke test |#
+  #| test that the first step in compilation workds
+   | even when used in a totally different file (importing the macros)
+   |#
   (check-equal?
    (list
     (ASL $10)
@@ -13,6 +16,10 @@
     (BRK)
     (JMP $FFD2)
     (JMP ($FFFE))
+    (JMP (some))
+    (SBC some)
+    (SBC >some)
+    (BEQ some)
     (NOP)
     (SBC $10,x)
     (STX $3A,y))
@@ -21,6 +28,11 @@
      (opcode 0)
      (opcode #x4c #xD2 #xFF)
      (opcode #x6c #xFE #xFF)
+     (opcode #x6c (resolve-word "some"))
+     (decide (((resolve-byte "some") opcode 229)
+              ((resolve-word "some") opcode 237)))
+     (opcode 229 (resolve-byte ">some"))
+     (rel-opcode 240 (resolve-relative "some"))
      (opcode 234)
      (opcode 245 16)
      (opcode 150 58))))
