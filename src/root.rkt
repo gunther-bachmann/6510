27225cf4608a1f44267451dbf331eb832be3e1ff
Gunther Bachmann <gunther.bachmann@itemis.de>
2025-11-08 09:37:29 +0100

    configure root depending on repo folder

--- src/root.rkt
+++ src/root.rkt
@@ -3,4 +3,5 @@
 (provide project-root)
 
 ;; needed by reader to calculate the actual module path to require into the generated code
+;; THIS FILE IS AUTOMATICALLY MODIFIED BY DIRENV, DON'T CHANCE THE FOLLOWING DEFINITION
 (define project-root (expand-user-path (string->path "~/repo/+1/6510/")))
